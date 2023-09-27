9577454be3ca1f37f2b349a670dd7c3ab4696277
Adalid Yujra <adalidirin@gmail.com>
2023-09-27 01:41:54 -0400

    Introducción a los arreglos y métodos

---
+++ arreglos.py
@@ -0,0 +1,20 @@
+personas=["adalid",30,"irvin"]
+print(personas)
+print(len(personas))
+
+for x in personas:
+  print(x)
+
+num=[1,3,2]
+num.sort()
+print(num)
+
+num2=[7,34,12]
+num.append(num2)
+print(num)
+print(len(num))
+
+num3=[1,3,2,2,3,1,5,1,2,7,23,1,431,21,1,23,12,1,2]
+print(num3.count(1))
+num3.reverse()
+print(num3)
